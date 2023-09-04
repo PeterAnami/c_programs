ca11b0b915b5d0aec5165018337abb47bd7be147
PeterAnami <andakepe@ueab.ac.ke>
2023-09-04 20:53:56 +0300

    while loop statements

---
+++ loops/while_loop/while1.c
@@ -0,0 +1,11 @@
+//program of while loop that prints table of 1
+
+#include <stdio.h>
+int main(){
+	int i=1;
+	while (i<=10){
+		printf("%d \n",i);
+		i++;
+	}
+	return 0;
+}
---
+++ loops/while_loop/while2.c
@@ -0,0 +1,13 @@
+// program to print a table for the given number using while loop in c
+
+#include <stdio.h>
+int main(){
+	int i= 1, number=0, b=9;
+	printf("Enter a number:");
+	scanf("%d",&number);
+	while (i<=10){
+		printf("%d \n",(number*i));
+		i++;
+	}
+	return 0;
+}
